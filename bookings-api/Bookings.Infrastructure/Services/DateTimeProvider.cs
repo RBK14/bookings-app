70652bafa8d584742749612618e66e7794594cfc
RBK14 <maciek.rybka2004@gmail.com>
2025-08-22 14:48:08 +0200

    feat(infrastructure): add DateTimeProvider implementation

---
+++ bookings-api/Bookings.Infrastructure/Services/DateTimeProvider.cs
@@ -0,0 +1,9 @@
+﻿using Bookings.Application.Common.Interfaces.Authentication;
+
+namespace Bookings.Infrastructure.Services
+{
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime UtcNow => DateTime.UtcNow;
+    }
+}
