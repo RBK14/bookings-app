19d63585bd9e6a323836d2b9f8187cd8fca40920
RBK14 <maciek.rybka2004@gmail.com>
2025-08-22 12:07:33 +0200

    feat(api): add authentication controller with endpoints

---
+++ bookings-api/Bookings.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,54 @@
+﻿using Bookings.Application.Services.Authentication;
+using Bookings.Contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookings.Api.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuthenticationController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequest request)
+        {
+            var authResult = _authenticationService.Register(
+                request.Name,
+                request.Phone,
+                request.Email,
+                request.Password);
+
+            var response = new AuthenticationResponse(
+                authResult.Id,
+                authResult.Name,
+                authResult.Phone,
+                authResult.Email,
+                authResult.Token);
+
+            return Ok(response);
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequest request)
+        {
+            var authResult = _authenticationService.Login(
+                request.Email,
+                request.Password);
+
+            var response = new AuthenticationResponse(
+                authResult.Id,
+                authResult.Name,
+                authResult.Phone,
+                authResult.Email,
+                authResult.Token);
+
+            return Ok(response);
+        }
+    }
+}
