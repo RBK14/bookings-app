294d054e9de9ebf58bcefad17857aabdafa8951f
RBK14 <maciek.rybka2004@gmail.com>
2025-08-22 12:08:27 +0200

    feat(core): add dependency injection setup for application and infrastructure layers

---
+++ bookings-api/Bookings.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Bookings.Application.Services.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bookings.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+            return services;
+        }
+    }
+}
