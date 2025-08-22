94c120fff6fb7cdbe6a3228b798a12c58668c6af
RBK14 <maciek.rybka2004@gmail.com>
2025-08-22 14:49:17 +0200

    chore(infrastructure,api): configure dependency injection and update project setup

--- bookings-api/Bookings.Infrastructure/DependencyInjection.cs
+++ bookings-api/Bookings.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Bookings.Application.Common.Interfaces.Authentication;
+using Bookings.Infrastructure.Authentication;
+using Bookings.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Bookings.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            ConfigurationManager configuration)
         {
+            services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+            services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+            services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
             return services;
         }
     }
