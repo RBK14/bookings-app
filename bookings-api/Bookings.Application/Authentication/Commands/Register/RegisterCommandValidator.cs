f688d79deadc08cf5502a2da4eaf76f308bf26d5
RBK14 <maciek.rybka2004@gmail.com>
2025-08-24 21:33:57 +0200

    feat(application): add FluentValidation validators for authentication commands and queries

---
+++ bookings-api/Bookings.Application/Authentication/Commands/Register/RegisterCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Bookings.Application.Authentication.Commands.Register
+{
+    public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+    {
+        public RegisterCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Phone).NotEmpty();
+            RuleFor(x => x.Email).NotEmpty();
+            RuleFor(x => x.Password).NotEmpty();
+        }
+    }
+}
