e02c87eec344221bf08ea7e178558d325fe65842
RBK14 <maciek.rybka2004@gmail.com>
2025-08-24 21:34:19 +0200

    chore(application): register FluentValidation in dependency injection

--- bookings-api/Bookings.Application/DependencyInjection.cs
+++ bookings-api/Bookings.Application/DependencyInjection.cs
@@ -1,5 +1,12 @@
 ﻿
+using Bookings.Application.Authentication.Commands.Register;
+using Bookings.Application.Authentication.Common;
+using Bookings.Application.Common.Behaviors;
+using ErrorOr;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Bookings.Application
 {
@@ -10,6 +17,10 @@ namespace Bookings.Application
             services.AddMediatR(cfg =>
                 cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
 
+            services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            
             return services;
         }
     }
