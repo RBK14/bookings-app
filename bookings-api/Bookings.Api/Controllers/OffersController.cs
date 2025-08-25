cd65badce43625b9f260e42b576c77059b04e775
RBK14 <maciek.rybka2004@gmail.com>
2025-08-25 21:06:31 +0200

    feat(api): add OffersController with endpoints

---
+++ bookings-api/Bookings.Api/Controllers/OffersController.cs
@@ -0,0 +1,17 @@
+﻿using Bookings.Contracts.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookings.Api.Controllers
+{
+    [Route("[controller]")]
+    [Authorize]
+    public class OffersController : ApiController
+    {
+        [HttpGet]
+        public IActionResult ListOffers()
+        {
+            return Ok(Array.Empty<string>());
+        }
+    }
+}
