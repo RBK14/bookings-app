dd89e19bb29f1a858f1df3d390a7f371634006cb
RBK14 <maciek.rybka2004@gmail.com>
2025-08-27 21:56:09 +0200

    feat(infrastructure): add OfferRepository implementation

---
+++ bookings-api/Bookings.Infrastructure/Persistence/Repositories/OfferRepository.cs
@@ -0,0 +1,27 @@
+﻿using Bookings.Application.Common.Interfaces.Persistence;
+using Bookings.Domain.OfferAggregate;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookings.Infrastructure.Persistence.Repositories
+{
+    public class OfferRepository : IOfferRepository
+    {
+        private readonly BookingsDbContext _dbContext;
+
+        public OfferRepository(BookingsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddAsync(Offer offer)
+        {
+            await _dbContext.AddAsync(offer);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Offer>> GetOffersAsync()
+        {
+            return await _dbContext.Offers.ToListAsync();
+        }
+    }
+}
