2c4214f31744b813411a5a2f7276c9ce8718fbb5
RBK14 <maciek.rybka2004@gmail.com>
2025-08-27 21:54:51 +0200

    feat(api): add OfferMappingConfig and OfferController

---
+++ bookings-api/Bookings.Api/Common/Mapping/OfferMappingConfig.cs
@@ -0,0 +1,26 @@
+﻿using Bookings.Application.Offers.Commands.CreateOffer;
+using Bookings.Contracts.Offers;
+using Bookings.Domain.AppointmentAggregate.ValueObjects;
+using Bookings.Domain.OfferAggregate;
+using Mapster;
+
+namespace Bookings.Api.Common.Mapping
+{
+    public class OfferMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<(CreateOfferRequest Request, string EmployeeId), CreateOfferCommand>()
+                .Map(dest => dest.EmployeeId, src => src.EmployeeId)
+                .Map(dest => dest, src => src.Request);
+
+            config.NewConfig<Offer, OfferResponse>()
+                .Map(dest => dest.Id, src => src.Id.Value)
+                .Map(dest => dest.EmployeeId, src => src.EmployeeId.Value)
+                .Map(dest => dest.Amount, src => src.Price.Amount)
+                .Map(dest => dest.Currency, src => src.Price.Currency)
+                .Map(dest => dest.Duration, src => src.Duration.Value)
+                .Map(dest => dest.Appointments, src => src.AppointmentIds.Select(appointmentId => appointmentId.Value));
+        }
+    }
+}
--- bookings-api/Bookings.Api/Controllers/OffersController.cs
+++ bookings-api/Bookings.Api/Controllers/OffersController.cs
@@ -1,17 +1,62 @@
-﻿using Bookings.Contracts.Authentication;
+﻿using Bookings.Application.Offers.Commands.CreateOffer;
+using Bookings.Application.Offers.Queries.GetOffers;
+using Bookings.Contracts.Offers;
+using MapsterMapper;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Bookings.Api.Controllers
 {
-    [Route("[controller]")]
+    [Route("offers")]
     [Authorize]
     public class OffersController : ApiController
     {
-        [HttpGet]
-        public IActionResult ListOffers()
+        private readonly ISender _mediator;
+        private readonly IMapper _mapper;
+
+        public OffersController(ISender mediator, IMapper mapper)
         {
-            return Ok(Array.Empty<string>());
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateOffer(CreateOfferRequest request)
+        {
+            // TODO: Przerobić na EmployeeId
+            var employeeId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            // TODO: Walidacja Id
+            if (string.IsNullOrEmpty(employeeId))
+                return Unauthorized();
+
+            var command = _mapper.Map<CreateOfferCommand>((request, employeeId));
+
+            var result = await _mediator.Send(command);
+
+            return result.Match(
+                offer => Ok(_mapper.Map<OfferResponse>(offer)),
+                errors => Problem(errors));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetOffers()
+        {
+            var query = new GetOffersQuery();
+
+            var offers = await _mediator.Send(query);
+
+            var offerResponses = new List<OfferResponse>();
+
+            foreach (var offer in offers)
+            {
+                var offerResponse = _mapper.Map<OfferResponse>(offer);
+                offerResponses.Add(offerResponse);
+            }
+
+            return Ok(offerResponses);
         }
     }
 }
