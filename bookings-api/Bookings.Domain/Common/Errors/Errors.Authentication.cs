f338084b94b7a83c020fc95f53dc1b7c0dfabc16
RBK14 <maciek.rybka2004@gmail.com>
2025-08-27 21:55:40 +0200

    refactor(domain): Error class fix

--- bookings-api/Bookings.Domain/Common/Errors/Errors.Authentication.cs
+++ bookings-api/Bookings.Domain/Common/Errors/Errors.Authentication.cs
@@ -1,14 +1,14 @@
 ﻿using ErrorOr;
 
-namespace Bookings.Domain.Common.Errors;
-
-public static partial class Errors
+namespace Bookings.Domain.Common.Errors
 {
-    public static class Authentication
+    public static partial class Errors
     {
-        public static Error InvalidCredentials => Error.Validation(
-            code: "Auth.InvalidCredentials",
-            description: "Invalid email of password.");
+        public static class Authentication
+        {
+            public static Error InvalidCredentials => Error.Validation(
+                code: "Auth.InvalidCredentials",
+                description: "Nieprawidłowy email lub hasło.");
+        }
     }
 }
-
--- bookings-api/Bookings.Domain/Common/Errors/Errors.User.cs
+++ bookings-api/Bookings.Domain/Common/Errors/Errors.User.cs
@@ -1,14 +1,16 @@
 ﻿using ErrorOr;
 
-namespace Bookings.Domain.Common.Errors;
-
-public static partial class Errors
+namespace Bookings.Domain.Common.Errors
 {
-    public static class User
+    public static partial class Errors
     {
-        public static Error DuplicateEmail => Error.Conflict(
-            code: "User.DuplicateEmail",
-            description: "User with given email already exists.");
+        public static class User
+        {
+            public static Error DuplicateEmail => Error.Conflict(
+                code: "User.DuplicateEmail",
+                description: "Użytownik z takim adresem email już istnieje.");
+        }
     }
 }
+
 
